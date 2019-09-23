c00ecaf89c62af79944c3a2b291173960b2d7eee
github username <github email address>
2019-09-23 11:02:59 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  config.web_console.whitelisted_ips = '127.0.0.1:9393'
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
